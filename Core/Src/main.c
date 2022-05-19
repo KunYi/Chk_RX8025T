8b9f5394ad7dbe2be2e1a1d1e89737bbc159b165
KunYi Chen/陳坤毅 <kunyi.chen@gmail.com>
2022-05-19 16:43:03 +0800

    initial version for verify Seiko/Epson RX-8025T
    Hardward configuration of board
    
    RTC(RX8025T): connect with GPIOB.13(SDA)/GPIOB.14(SCL)
    SWD: enabled:
    UART1: GPIOA.9/GPIOA.10
    HSE: Crystal/8 MHz

---
+++ Core/Src/main.c
@@ -0,0 +1,302 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include <stdlib.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+uint8_t data;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
+
+/* USER CODE BEGIN PFP */
+
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif
+
+void rtcBusInit(void);
+uint8_t rtcI2CWriteByte(uint8_t addr, uint8_t reg, uint8_t val);
+uint8_t rtcI2CReadByte(uint8_t addr, uint8_t reg);
+uint8_t rtcSimpleReadByte(uint8_t addr, uint8_t reg);
+uint8_t chkDevOnBus(uint8_t addr);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+PUTCHAR_PROTOTYPE {
+	if (ch == '\n')
+		HAL_UART_Transmit(&huart1, "\r\n", 2, 1000);
+	else
+		HAL_UART_Transmit(&huart1, (uint8_t*)&ch, 1, 1000);
+  return ch;
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+	printf("\n\nCheck RTC Bus\n");
+
+	rtcBusInit();
+	for (uint8_t devAddr = 0; devAddr < 0x7F; devAddr++) {
+		 	if (chkDevOnBus(devAddr)) {
+         printf("Found device on addr: 0x%02x\n", devAddr);
+			}
+	}
+	data = rtcI2CReadByte(0x32, 0xE0);
+  printf("Address: 0x0E, value:0x%02x\n", data);
+	data = rtcI2CReadByte(0x32, 0xF0);
+  printf("Address: 0x0F, value:0x%02x\n", data);
+  data = rtcSimpleReadByte(0x32, 0x0E);
+  printf("Address: 0x0E, value:0x%02x\n", data);
+  data = rtcSimpleReadByte(0x32, 0x0F);
+  printf("Address: 0x0F, value:0x%02x\n", data);
+  rtcI2CWriteByte(0x32, 0x0E, (uint8_t)1 << 5);
+  rtcI2CWriteByte(0x32, 0x0F, ((uint8_t)1 << 5) + ((uint8_t)1 << 7));
+
+  rtcI2CWriteByte(0x32, 0, 0);
+  rtcI2CWriteByte(0x32, 1, 0);
+  rtcI2CWriteByte(0x32, 2, 0);
+  data = rtcSimpleReadByte(0x32, 0x00);
+  printf("Address: 0x00, value:0x%02x\n", data);
+  data = rtcSimpleReadByte(0x32, 0x01);
+  printf("Address: 0x00, value:0x%02x\n", data);
+  data = rtcSimpleReadByte(0x32, 0x0E);
+  printf("Address: 0x0E, value:0x%02x\n", data);
+  data = rtcSimpleReadByte(0x32, 0x0F);
+  printf("Address: 0x0F, value:0x%02x\n", data);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LED1_Pin|RTC_SCL_Pin|RTC_SDA_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin : LED2_Pin */
+  GPIO_InitStruct.Pin = LED2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LED1_Pin */
+  GPIO_InitStruct.Pin = LED1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : RTC_SCL_Pin RTC_SDA_Pin */
+  GPIO_InitStruct.Pin = RTC_SCL_Pin|RTC_SDA_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ Core/Src/sw_i2c.c
@@ -0,0 +1,343 @@
+
+#include "stm32f1xx_hal.h"
+#include <stdint.h>
+
+
+#define CMD_WRITE       (0)
+#define CMD_READ        (1)
+#define SCL_Pin         GPIO_PIN_14
+#define SCL_GPIO_Port   GPIOB
+#define SDA_Pin         GPIO_PIN_13
+#define SDA_GPIO_Port   GPIOB
+
+#define SCL_L()         HAL_GPIO_WritePin(SCL_GPIO_Port, SCL_Pin, GPIO_PIN_RESET)
+#define SCL_H()         HAL_GPIO_WritePin(SCL_GPIO_Port, SCL_Pin, GPIO_PIN_SET)
+#define READ_SCL()      (HAL_GPIO_ReadPin(SCL_GPIO_Port, SCL_Pin) == GPIO_PIN_SET)
+#define SDA_L()         HAL_GPIO_WritePin(SDA_GPIO_Port, SDA_Pin, GPIO_PIN_RESET)
+#define SDA_H()         HAL_GPIO_WritePin(SDA_GPIO_Port, SDA_Pin, GPIO_PIN_SET)
+#define READ_SDA()      (HAL_GPIO_ReadPin(SDA_GPIO_Port, SDA_Pin) == GPIO_PIN_SET)
+
+#define set_SCL()       HAL_GPIO_WritePin(SCL_GPIO_Port, SCL_Pin, GPIO_PIN_SET)
+#define clear_SCL()     HAL_GPIO_WritePin(SCL_GPIO_Port, SCL_Pin, GPIO_PIN_RESET)
+#define read_SCL()      (HAL_GPIO_ReadPin(SCL_GPIO_Port, SCL_Pin) == GPIO_PIN_SET)
+#define set_SDA()       HAL_GPIO_WritePin(SDA_GPIO_Port, SDA_Pin, GPIO_PIN_SET)
+#define clear_SDA()     HAL_GPIO_WritePin(SDA_GPIO_Port, SDA_Pin, GPIO_PIN_RESET)
+#define read_SDA()      (HAL_GPIO_ReadPin(SDA_GPIO_Port, SDA_Pin) == GPIO_PIN_SET)
+#define I2C_delay()     delayUs(25)
+
+#define delayParameters (200)
+static uint8_t started = 0;
+
+static void delayUs(volatile uint32_t nCount) {
+  for (; nCount != 0; nCount--)
+    ;
+}
+
+void rtcBusInit(void) {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  SDA_H();
+  SCL_H();
+
+  /*Configure GPIO pins : SCL_Pin and SDA_Pin */
+  GPIO_InitStruct.Pin = SCL_Pin | SDA_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+}
+
+void i2c_start_cond(void) {
+  if (started) {
+    // if started, do a restart condition
+    // set SDA to 1
+    set_SDA();
+    I2C_delay();
+    set_SCL();
+    while (read_SCL() == 0) { // Clock stretching
+      // You should add timeout to this loop
+    }
+    // Repeated start setup time, minimum 4.7us
+    I2C_delay();
+  }
+
+  if (read_SDA() == 0) {
+    // arbitration_lost();
+  }
+
+  // SCL is high, set SDA from 1 to 0.
+  clear_SDA();
+  I2C_delay();
+  clear_SCL();
+  started = 1;
+}
+
+void i2c_stop_cond(void) {
+  // set SDA to 0
+  clear_SDA();
+  I2C_delay();
+
+  set_SCL();
+  // Clock stretching
+  while (read_SCL() == 0) {
+    // add timeout to this loop.
+  }
+
+  // Stop bit setup time, minimum 4us
+  I2C_delay();
+
+  // SCL is high, set SDA from 0 to 1
+  set_SDA();
+  I2C_delay();
+
+  if (read_SDA() == 0) {
+    // arbitration_lost();
+  }
+
+  started = 0;
+}
+
+// Write a bit to I2C bus
+void i2c_write_bit(uint8_t bit) {
+  if (bit) {
+    set_SDA();
+  } else {
+    clear_SDA();
+  }
+
+  // SDA change propagation delay
+  I2C_delay();
+
+  // Set SCL high to indicate a new valid SDA value is available
+  set_SCL();
+
+  // Wait for SDA value to be read by target, minimum of 4us for standard mode
+  I2C_delay();
+
+  while (read_SCL() == 0) { // Clock stretching
+    // You should add timeout to this loop
+  }
+
+  // SCL is high, now data is valid
+  // If SDA is high, check that nobody else is driving SDA
+  if (bit && (read_SDA() == 0)) {
+    // arbitration_lost();
+  }
+
+  // Clear the SCL to low in preparation for next change
+  clear_SCL();
+}
+
+// Read a bit from I2C bus
+uint8_t i2c_read_bit(void) {
+  uint8_t bit;
+
+  // Let the target drive data
+  set_SDA();
+
+  // Wait for SDA value to be written by target, minimum of 4us for standard
+  // mode
+  I2C_delay();
+
+  // Set SCL high to indicate a new valid SDA value is available
+  set_SCL();
+
+  while (read_SCL() == 0) { // Clock stretching
+    // You should add timeout to this loop
+  }
+
+  // Wait for SDA value to be written by target, minimum of 4us for standard
+  // mode
+  I2C_delay();
+
+  // SCL is high, read out bit
+  bit = read_SDA();
+
+  // Set SCL low in preparation for next operation
+  clear_SCL();
+
+  return bit;
+}
+
+// Write a byte to I2C bus. Return 0 if ack by the target.
+uint8_t i2c_write_byte(uint8_t send_start, uint8_t send_stop,
+                       uint8_t byte) {
+  uint8_t bit;
+  uint8_t nack;
+
+  if (send_start) {
+    i2c_start_cond();
+  }
+
+  for (bit = 0; bit < 8; ++bit) {
+    i2c_write_bit((byte & 0x80) != 0);
+    byte <<= 1;
+  }
+
+  nack = i2c_read_bit();
+
+  if (send_stop) {
+    i2c_stop_cond();
+  }
+
+  return nack;
+}
+
+// Read a byte from I2C bus
+uint8_t i2c_read_byte(uint8_t nack, uint8_t send_stop) {
+  uint8_t byte = 0;
+  uint8_t bit;
+
+  for (bit = 0; bit < 8; ++bit) {
+    byte = (byte << 1) | i2c_read_bit();
+  }
+
+  i2c_write_bit(nack);
+
+  if (send_stop) {
+    i2c_stop_cond();
+  }
+
+  return byte;
+}
+#if 0
+static void rtcI2CStart(void) {
+  SDA_H();
+  SCL_H();
+  delayUs(delayParameters);
+  SDA_L();
+  delayUs(delayParameters);
+}
+
+static void rtcI2CStop(void) {
+  SDA_L();
+  SCL_H();
+  delayUs(delayParameters);
+  SDA_H();
+  delayUs(delayParameters);
+}
+
+static uint8_t rtcI2CAck(void) {
+  int i;
+  // RTC_SDA_IN_MODE();
+  SCL_L();
+  delayUs(delayParameters);
+  SCL_H();
+  delayUs(delayParameters);
+  for (i = 0; i < 20; i++) {
+    if (READ_SDA() == 0)
+      return 1;
+  }
+  return 0;
+}
+
+static void rtcI2CSendNack(void) {
+  SDA_H();
+  SCL_L();
+  delayUs(delayParameters);
+  SCL_H();
+  delayUs(delayParameters);
+}
+
+void rtcI2CDataOut(uint8_t data) {
+  for (int i = 0; i < 8; i++) {
+    if ((data & (0x80 >> i)) != 0)
+      SDA_H();
+    else
+      SDA_L();
+    SCL_L();
+    delayUs(delayParameters);
+    SCL_H();
+    delayUs(delayParameters);
+  }
+}
+
+uint8_t rtcI2CDataIn(void) {
+  // uint8_t result = 0;
+  uint8_t i;
+  uint8_t d;
+  d = 0;
+
+  SDA_H();
+  for (i = 0; i < 8; i++) {
+    SCL_L();
+    delayUs(delayParameters);
+    SCL_H();
+    delayUs(delayParameters);
+    if (READ_SDA())
+      d++;
+    d <<= 1;
+  }
+  // RTC_SDA_OUT_MODE();
+  delayUs(delayParameters);
+  return d;
+}
+#endif
+
+uint8_t rtcI2CWriteByte(uint8_t addr, uint8_t reg, uint8_t val) {
+  uint8_t ack;
+  ack = i2c_write_byte(1, 0, (addr << 1) + CMD_WRITE);
+  if (ack) {
+    while(1); // failed
+  }
+
+  ack = i2c_write_byte(0, 0, reg << 4);
+  if (ack) {
+    while(1); // failed
+  }
+
+  ack = i2c_write_byte(0, 1, val);
+  if (ack) {
+    while(1); // failed
+  }
+  return 1;
+}
+
+uint8_t rtcI2CReadByte(uint8_t addr, uint8_t reg) {
+  uint8_t result;
+  uint8_t ack;
+
+  ack = i2c_write_byte(1, 0, (addr << 1) + CMD_WRITE);
+  if (ack) {
+    while(1); // failed
+  }
+
+  ack = i2c_write_byte(0, 0, reg);
+  if (ack) {
+    while(1); // failed
+  }
+
+  ack = i2c_write_byte(1, 0, (addr << 1) + CMD_READ);
+  if (ack) {
+    while(1); // failed
+  }
+
+  result = i2c_read_byte(1, 1);
+  return result;
+}
+
+uint8_t rtcSimpleReadByte(uint8_t addr, uint8_t reg)
+{
+  uint8_t result;
+  uint8_t ack;
+  ack = i2c_write_byte(1, 0, (addr << 1) + CMD_WRITE);
+  if (ack) {
+    while(1); // failed
+  }
+  ack = i2c_write_byte(0, 0, (reg << 4) + 4); // for simple read
+  if (ack) {
+    while(1); // failed
+  }
+
+  result = i2c_read_byte(1, 1);
+  return result;
+}
+
+uint8_t chkDevOnBus(uint8_t addr)
+{
+  uint8_t ack;
+  ack = i2c_write_byte(1, 0, (addr << 1) + CMD_WRITE);
+	i2c_stop_cond();
+  return (ack) ? 0 : 1;
+}
